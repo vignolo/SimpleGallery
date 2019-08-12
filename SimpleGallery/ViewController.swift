dc7a1f5e64cf19830798fe4802bfa55af26a9ee0
Nicolas Vignolo <vignolo@gmail.com>
2019-08-12 15:57:14 -0300

    Initial Commit

---
+++ SimpleGallery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleGallery
+//
+//  Created by Nicolas Vignolo on 12/08/2019.
+//  Copyright © 2019 Nicolas Vignolo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
