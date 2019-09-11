039c11ec2d3101bcc153957489a357c882936346
Nicolas Vignolo <vignolo@gmail.com>
2019-09-10 22:33:30 -0300

    SimpleGalleryTests: added testStoredCredentials, testWrongLoginCredentials

--- SimpleGalleryTests/SimpleGalleryTests.swift
+++ SimpleGalleryTests/SimpleGalleryTests.swift
@@ -10,25 +10,36 @@ import XCTest
 @testable import SimpleGallery
 
 class SimpleGalleryTests: XCTestCase {
+    
+    var session:Session!
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+        session = Session()
+        
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        session = nil
+       super.tearDown()
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testStoredCredentials() {
+        session.signOut()
+        XCTAssertNil(session.user)
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func testWrongLoginCredentials() {
+        let promise = expectation(description: "Login fail")
+        session.signIn(with: "worng@gmail.com", password: "password") { (success, error) in
+            if success {
+                XCTFail("Login succeed")
+            } else {
+                promise.fulfill()
+            }
         }
+        
+        wait(for: [promise], timeout: 5)
     }
 
 }
